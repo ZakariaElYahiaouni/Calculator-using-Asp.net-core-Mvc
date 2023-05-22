83cbe55dfbcec71ec78ea4355e3231b4e7469ab0
ZakariaElYahiaouni <68422258+ZakariaElYahiaouni@users.noreply.github.com>
2023-05-22 12:40:48 +0200

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace myFirstAspApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
